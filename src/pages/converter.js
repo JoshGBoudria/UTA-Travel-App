404e1cd092bfc1509ce349c05aaf5c2f98099b88
brysonneel1999 <70408495+brysonneel1999@users.noreply.github.com>
2022-09-26 12:09:51 -0600

    Currency Converter Updated

--- src/pages/converter.js
+++ src/pages/converter.js
@@ -4,24 +4,62 @@
  */
 
 import React from "react";
+import { useLinkClickHandler } from "react-router-dom";
 import '../App.css';
 
+function clickHandler() {
+	const convert = document.getElementById("convert");
+	const result = document.getElementById("result");
+	const from = document.getElementById("from");
+	const to = document.getElementById("to");
+	const amount = document.getElementById("amount");
+
+	let fromCurrency = from.value;
+	let toCurrency = to.value;
+	let amt = amount.value;
+	result.innerHTML = `${from} + ${to}`;
+	fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			let rate = data.rates[toCurrency];
+			console.log(rate);
+			let total = rate * amt;
+			result.innerHTML = `${amt} ${fromCurrency} = ${total} ${toCurrency}`;
+		});
+}
+
 const Converter = () => {
-  return (
-    <div class="App-Converter">
-        <h1> Currency Converter </h1>
-        <div>
-            <p> YEN to USD </p>
-            <input type="text" id="USD" placeholder="Enter USD" maxlength="15"/>
-            <button id="USDConv">Convert</button>
-            </div>
-        <div>
-            <p> USD to YEN </p>
-            <input type="text" id="YEN" placeholder="Enter YEN" maxlength="15"/>
-            <button id="YENConv">Convert</button>
-            </div>
-    </div>
+	return (
+		<div class="App-Converter">
+			<head>
+				<link rel="shortcut icon" href="#" />
+				<title>Currency Converter</title>
+			</head>
+			<body>
+				<h1>Currency Converter</h1>
+				<form>
+					<label>From:</label>
+					<select id="from">
+						<option value="JPY">Japanese Yen</option>
+						<option value="USD">US Dollars</option>
+					</select>
+					<label>To:</label>
+					<select id="to">
+						<option value="USD">US Dollars</option>
+						<option value="JPY">Japanese Yen</option>
+					</select>
+					<label>Amount:</label>
+					<input type="text" id="amount" />
+					<button onClick={clickHandler} type="button" id="convert">Convert</button>
+					<p id="result"></p>
+				</form>
+			  </body>
+		</div>
     );
 };
+
+
   
 export default Converter;
