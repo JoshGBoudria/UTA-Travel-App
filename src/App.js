071439266fdf94a0ce2f13b3aa420a16afd6af3b
brysonneel1999 <70408495+brysonneel1999@users.noreply.github.com>
2022-10-09 20:37:00 -0600

    Code login and icons 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Routes, Route} 
