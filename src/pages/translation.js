6371ca3f015cc262a7cf0410e73a565140884fc8
shane272001 <95012994+shane272001@users.noreply.github.com>
2022-10-17 18:31:11 -0500

    Update translation.js

--- src/pages/translation.js
+++ src/pages/translation.js
@@ -38,6 +38,176 @@ const Translation = () =>
 					</p>
 					{/* TODO: Display the rest of the translations */}
 				</li>
+                                <li>
+					Good Bye!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Good-Bye.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Good Evening!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Good-Evening.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Good Morning!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Good-Morning.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Hello!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Hello.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					I am sorry!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/I-Am-Sorry.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					I do not understand.
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/I-Do-Not-Understand.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					My name is...
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/My-Name-Is.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					No.
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/No.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Please!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Please.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Please tell me your name!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Please-Tell-Me-Your-Name.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					See you later!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/See-You-Later.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Thanks for what you did!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Thanks-For-What-You-Did.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Thank you!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Thank-You.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					That is alright!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/That-Is-Alright.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Where is restroom?
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Where-Is-Restroom.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					Yes.
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/Yes.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
+                                <li>
+					You are welcome!
+					<p style={{ textAlign: 'center' }}>
+						<audio controls>
+							<source src={require("../translations/You-Are-Welcome.mp3")} type="audio/mpeg" />
+							Your browser does not support HTML5 audio.
+						</audio>
+					</p>
+					{/* TODO: Display the rest of the translations */}
+				</li>
 			</ul >
 		</>
 	);
