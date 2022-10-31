3c6466dfb58b043e0709f1b3962ece13442ef203
brysonneel1999 <70408495+brysonneel1999@users.noreply.github.com>
2022-10-31 14:36:26 -0600

    dropbox tweaks

--- src/pages/dropbox.js
+++ src/pages/dropbox.js
@@ -6,13 +6,48 @@
  */
 
 
-import React, { useState } from "react";
+import React/*, { useState }*/ from "react";
 import { useDropzone } from 'react-dropzone';
 import Cookies from "js-cookie";
 
-
-const Dropbox = () =>
+const Dropbox = (props) =>
 {
+	const
+		{
+			acceptedFiles,
+			fileRejections,
+			getRootProps,
+			getInputProps
+		} = useDropzone({
+			// Users can only upload jpg and png images (later change to allow videos, too)
+			accept:
+			{
+				'image/jpg': [],
+				'image/png': []/*,
+				'video/mp4': [],
+				'video/mov': [],
+				'video/avi': []*/
+			},
+		});
+
+	const acceptedFileItems = acceptedFiles.map(file => (
+		<li key={file.path}>
+			{file.path} - {file.size} bytes
+		</li>
+	));
+
+	const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+		<li key={file.path}>
+			{file.path} - {file.size} bytes
+			<ul>
+				{errors.map(e => (
+					<li key={e.code}>{e.message}</li>
+				))}
+			</ul>
+		</li>
+	));
+
+
 
 	// Checks if the user entered the correct code
 	const check_code = () =>
@@ -54,39 +89,6 @@ const Dropbox = () =>
 		}
 	};
 
-	const [files, setFiles] = useState([]);
-	const { getRootProps, getInputProps } = useDropzone(
-		{
-			// Users can only upload images (might change this later)
-			accept: "image/*",
-			// Users can't upload more than 5MB at a time
-			maxSize: 5000000,
-			multiple: false,
-			onDrop: (acceptedFiles) =>
-			{
-				setFiles(
-					acceptedFiles.map((file) => Object.assign(file,
-						{
-							preview: URL.createObjectURL(file)
-						}))
-				)
-			},
-		});
-
-	const images = files.map((file) =>
-	(
-		<div key={file.name}>
-			<div>
-				{/* Display the image that was uploaded (later change it so that it just stores it
-					in the database) */}
-				<img src={file.preview} style={{
-					display: 'block', 'margin-left': 'auto',
-					'margin-right': 'auto', width: '400px', height: 'auto'
-				}} alt="preview" />
-			</div>
-		</div>
-	));
-
 	if (Cookies.get("code_cookie") === process.env.REACT_APP_LOGIN_CODE)
 	{
 		return (
@@ -94,18 +96,20 @@ const Dropbox = () =>
 				{/*The header for the Dropbox.*/}
 				<h1> Dropbox </h1>
 				{/* File upload */}
-				<div {...getRootProps()}>
+				<input {...getInputProps()} />
+				<div {...getRootProps({ className: 'dropzone' })}>
 					<input {...getInputProps()} />
 					{/* Display the upload icon */}
-					<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-upload" width="100" height="100" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+					<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-upload" width="100" height="100" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
 						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 						<path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
 						<polyline points="7 9 12 4 17 9" />
 						<line x1="12" y1="4" x2="12" y2="16" />
 					</svg>
-					<p>Tap to upload image</p>
+					<p>Tap to upload file</p>
+					<em>(.jpeg or .png)</em>
 				</div>
-				<div>{images}</div>
+				<div>{acceptedFileItems}</div>
 			</div>
 		);
 	}
@@ -115,7 +119,7 @@ const Dropbox = () =>
 		return (
 			<div>
 				<p id="ip_address"></p>
-				<label Text style={{ fontSize: 14 }}>
+				<label style={{ fontSize: 14 }}>
 					Enter code provided by professor to access the dropbox:&nbsp;
 				</label>
 				<input type="password" id="code" />
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 /* 
  * Shane Purdy 1001789955
  * Bryson Neel 1001627866
@@ -16,7 +18,7 @@ const Home = () =>
 				{/* Link the manifest.json file so that the PWA can be intalled */}
 				<link rel="manifest" href="manifest.json" />
 				<link rel="shortcut icon" href="#" />
-				<meta charset="UTF-8" />
+				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width = device-width, initial-scale = 1.0" />
 				<meta http-equiv="X-UA-Compatible" content="ie = edge" />
 				{/*<script src="app.js"> </script>*/}
