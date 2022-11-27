2c6affa8cf93861cf5acb112cb43c6f39828caae
JoshGBoudria <100064764+JoshGBoudria@users.noreply.github.com>
2022-11-27 17:58:04 -0600

    String input itinerary
    This is the itinerary that takes in string input and turns it into dates the DOES INCLUDE the time of day. 
    The other version that I am currently working on will utilize the datepicker functionality which will provide and easier user experience.

--- src/pages/itinerary.js
+++ src/pages/itinerary.js
@@ -4,21 +4,18 @@
  * Josh Boudria 1001826768
  */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { getDatabase, ref, child, push, update} from "firebase/database";
-import { initializeApp, /*applicationDefault, cert,*/ getApps, getApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { arrayUnion, getFirestore, updateDoc, addDoc, getDocs, where, collection, serverTimestamp, doc, onSnapshot, orderBy, query, QuerySnapshot } from 'firebase/firestore';
-import { getDownloadURL, getStorage, uploadBytes} from 'firebase/storage';
-//import { ErrorResponse } from "@remix-run/router";
+import { arrayUnion, getFirestore, updateDoc, addDoc, getDocs, where, collection, serverTimestamp, doc, onSnapshot, orderBy, query, QuerySnapshot, DocumentSnapshot } from 'firebase/firestore';
 
 //Establishes calendar to base on US date and time 
 const locales = {
@@ -40,22 +37,36 @@ const firebaseConfig =
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const analytics = getAnalytics(app);
 const db = getFirestore();
-const storage = getStorage();
+
+
+
 const dateRegExp = /^[0-9]{2}\-[0-9]{2}\-[0-9]{4}/;
+const timeRegExp = /^[0-1][0-9]\:[0-9]{2}/;
 async function createNewEvent(){
 	if(dateRegExp.test(document.getElementById("Start").value) && dateRegExp.test(document.getElementById("End").value)){
-		const eventRef = await addDoc(collection(db, 'Events'),{
-			TITLE: document.getElementById("Title").value,
-			START: document.getElementById("Start").value,
-			END: document.getElementById("End").value,
-			DESCRIP: document.getElementById("Descrip").value
-		});
+		if(timeRegExp.test(document.getElementById("Time").value)){
+			const startForm = dateFormat(document.getElementById("Start").value, document.getElementById("Time").value);
+			const endForm = dateFormat(document.getElementById("End").value, document.getElementById("Time").value);
+			const eventRef = await addDoc(collection(db, 'Events'),{
+				TITLE: document.getElementById("Title").value,
+				START: startForm,
+				END: endForm
+			});
+		}
+		else{
+			alert("Time format incorrect! Format: HH:MM")
+		}
 	}
 	else{
 		alert("Date format incorrect! Format: MM-DD-YYYY")
 	}
+}
+
+function dateFormat(dateString, timeString) {
+	const sepDate = dateString.split("-");
+	const finalString = sepDate[2] + "-" + sepDate[0] + "-" + sepDate[1] + "T" + timeString + ":00";
+	return finalString;
 }
 
 // calculates the current date and creates buffer dates for month view
@@ -67,18 +78,39 @@ const localizer = dateFnsLocalizer({
 	locales
 })
 
-//List of events that is currently just a local array. Going to attach this to the database in iteration 3
-const events = [];
 //Function to display the calendar
 function Calendarfxn() {
+	const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""});
+	const [allEvents, setAllEvents] = useState([]);
 
-	const [newEvent, setNewEvent] = useState({title: "", start: "", end: "", description: ""});
-	const [allEvents, setAllEvents] = useState(events);
+	// const fetchEvents = async () => {
+	// 	await getDocs(collection(db, "Events")).then((QuerySnapshot) => {
+	// 		const newData = QuerySnapshot.docs.map((doc) => ({...doc.data(), id:doc.id }));
+	// 		setNewEvent({title: });
+			
+	// 	})
+	// }
 
-	//Function that creates new event and adds it to the list of allEvents
-	function handleAddEvent(){
-		setAllEvents([...allEvents, newEvent])
+	const fetchEvents = async () => {
+		var eventsTemp = [];
+		await getDocs(collection(db, "Events")).then((QuerySnapshot) => {
+			QuerySnapshot.forEach(DocumentSnapshot => {
+				const eventObj = {
+					title: DocumentSnapshot.get("TITLE"),
+					start: new Date(DocumentSnapshot.get("START")),
+					end: new Date(DocumentSnapshot.get("END"))
+				}
+				eventsTemp = [...eventsTemp, eventObj];
+			});
+			setAllEvents(eventsTemp);
+		});
 	}
+	
+	useEffect(()=>{
+		fetchEvents();
+	}, [])
+	
+	window.addEventListener('load', fetchEvents);
 
 	//Formatting/UI of the calendar and add event functionalities
 	return(
@@ -87,9 +119,9 @@ function Calendarfxn() {
 			<h2>Add New Event</h2>
 			<div>
 				<input type="text" id="Title"  placeholder="Add Title" style={{width: "20%", marginRight: "10px"}}/>
-				<input type="text" id="Descrip" placeholder="Add Description" style={{width: "20%", marginRight: "10px"}}/>
 				<input type="text" id="Start" placeholder="Start Date (MM-DD-YYYY)" style={{width: "20%", marginRight: "10px"}}/>
 				<input type="text" id="End" placeholder="End Date (MM-DD-YYYY)" style={{width: "20%", marginRight: "10px"}}/>
+				<input type="text" id="Time" placeholder="Time HH:MM" style={{width: "20%", marginRight: "10px"}}/>
 				<button style={{margintop: "10px"}} 
 				onClick={createNewEvent}>Submit</button>
 			</div>
